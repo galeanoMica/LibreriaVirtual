4c025d042d967955afb852583c561d6a6b4f3f90
Karina Molina <karina_soledad_023@hotmail.com>
2023-04-22 15:16:15 -0300

    Inicio de carrito de compra

---
+++ src/libreriaVirtual/CarritoDecompras.java
@@ -0,0 +1,13 @@
+package libreriaVirtual;
+
+public class CarritoDecompras {
+
+	private ArrayList<itemsCarrito>items;
+	
+	
+	public CarritoDecompras() {
+         this.items = new ArrayList<itemsCarrito>();		
+		
+	}
+	
+}
