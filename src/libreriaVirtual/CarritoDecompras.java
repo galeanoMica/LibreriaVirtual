005245944d836eddc934d248b78159e1b553365b
Karina Molina <karina_soledad_023@hotmail.com>
2023-04-23 17:54:28 -0300

    Implementacion de carrito de compras

--- src/libreriaVirtual/CarritoDecompras.java
+++ src/libreriaVirtual/CarritoDecompras.java
@@ -1,5 +1,7 @@
 package libreriaVirtual;
 
+import ar.edu.utn.ItemCarrito;
+
 public class CarritoDecompras {
 
 	private ArrayList<itemsCarrito>items;
@@ -8,6 +10,27 @@ public class CarritoDecompras {
 	public CarritoDecompras() {
          this.items = new ArrayList<itemsCarrito>();		
 		
+	}
+	
+	public void agregarItem(ItemCarrito itemc) {
+		this.items.add(itemsC);
+	}
+	
+	
+	public void quitarItem(ItemCarrito ic) {
+		this.items.remove(ic);
+	}
+
+	
+	public void mostrarCarrito() {
+		if(items.isEmpty()) {
+			system.out.printl("No hay productos en el carrito");
+		
+		} else {
+			for (ItemCarrito i: items) {
+				i.mostrarItem();
+			}
+		}
 	}
 	
 }
